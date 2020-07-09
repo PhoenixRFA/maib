d529e0100dab29b5b270cee836e15a6f5780d1eb
Phoenix <gralukkr@gmail.com>
2020-07-09 17:12:32 +0300

    T59289 Правки по лендинг странице

--- src/script.js
+++ src/script.js
@@ -1,77 +1,78 @@
 $(document).ready(function () {
 
-  $('#enter_btn, .fill-profile-btn').click((e)=>{
-    e.preventDefault();
-    $('#form-modal').modal('show');
-  })
+    $('#enter_btn, .fill-profile-btn').click((e) => {
+        e.preventDefault();
+        $('#form-info').modal('show');
+    });
 
-  $(document).on('click', '#sendForm', function(e){
-    e.preventDefault();
-    var formData = new FormData($('#main-form')[0]);
-    var object = {};
-    formData.forEach((value, key) => {object[key] = value});
-    $.ajax({
-      url: 'https://c-rb.com/home/HandleMIABForm',
-      dataType: 'jsonp',
-      data: object,
-      method: 'get',
-      success: function(res){
-        $('#form-modal').modal('hide');
-      }
-    })
-  });
+    $('.open-main-form').click((e) => {
+        e.preventDefault();
+        $('#form-info').modal('hide')
+            .on('hidden.bs.modal', function() {
+                $('#form-modal').modal('show');
+            });
+    });
 
-  $(document).on('click', '#send-form-short', function(e){
-    e.preventDefault();
-    var formData = new FormData($('#form-short')[0]);
-    var object = {};
-    formData.forEach((value, key) => {object[key] = value});
-    $.ajax({
-      url: 'https://c-rb.com/home/HandleMIABForm',
-      dataType: 'jsonp',
-      data: object,
-      method: 'get',
-      success: function(res){}
-    })
-  });
+    $(document).on('click', '#sendForm', function (e) {
+        e.preventDefault();
+        $('#fmain-form').submit();
+    });
 
-  $('.first-button').on('click', function () {
+    $(document).on('click', '#send-form-short', function (e) {
+        e.preventDefault();
+        var formData = new FormData($('#form-short')[0]);
+        var object = {};
+        formData.forEach((value, key) => { object[key] = value });
+        $.ajax({
+            url: 'https://c-rb.com/home/HandleMIABForm',
+            dataType: 'jsonp',
+            data: object,
+            method: 'get',
+            success: function (res) { }
+        })
+    });
 
-    $('.animated-icon1').toggleClass('open');
-  });
+    $(document).on('click', '.file-dropdown', function(e) {
+        var $btn = $(e.target);
+        var fileType = $btn.data('file');
+
+        var $file = $('#file-' + fileType);
+        if ($file.length) $file.click();
+    });
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
 });
 
 
 
 var sampleEvents = [
     {
-      title: "forum",
-      date:  Date.parse( new Date(2019, 10, 2)),
-      link: "#"
+        title: "forum",
+        date: new Date(2020, 6, 2).getTime(),
+        link: "#"
     },
-	 {
-      title: "forum",
-      date:  new Date(2019, 10, 14), // last week
-      link: "#"
+    {
+        title: "forum",
+        date: new Date(2020, 6, 14).getTime(),
+        link: "#"
     },
-	 {
-      title: "forum",
-      date:  new Date(2019, 10, 19), // last week
-      link: "#"
+    {
+        title: "forum",
+        date: new Date(2020, 6, 19).getTime(),
+        link: "#"
     },
-	 {
-      title: "forum",
-      date: new Date(2019, 10, 29), // last week
-      link: "#"
+    {
+        title: "forum",
+        date: new Date(2020, 6, 29).getTime(),
+        link: "#"
     }
 ];
 
 $("#calendar").MEC({
-  events: sampleEvents
-});
-
-$("#calendar").MEC({
-  events: sampleEvents,
-  from_monday: true
+    events: sampleEvents,
+    from_monday: true
 });
 
